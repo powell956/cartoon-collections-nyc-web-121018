202e59e915e08e8a98440315efbbe74bb62ebc0d
powell956 <powell956@gmail.com>
2018-12-28 03:47:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,11 +10,20 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foodstuffs)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  has_cheese? = false
+  
+  cheese_types.each do |cheese|
+    has_cheese = has_cheese || foodstuffs.include?(cheese)
+  end
+
+  has_cheese
 end
